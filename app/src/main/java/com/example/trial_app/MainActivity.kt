34cb010350311f669b8b31343d613cd6001d78ba
Chelsea <65673755+Chelsea-N-K@users.noreply.github.com>
2024-06-19 11:58:01 +0300

    Complete Code

---
+++ app/src/main/java/com/example/trial_app/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.example.trial_app
+
+import android.content.Intent
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import com.example.trial_app.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //Initiate binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Initialize the listview and adapter with a non-null list
+        val usersList = listOf(User("John Doe"), User("Jane Doe"))
+        val adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,usersList.map { it.name })
+        binding.included.recyclerView.adapter = adapter
+        binding.included.recyclerView.onItemClickListener = AdapterView.OnItemClickListener { adapterView, view, i, l ->
+            val intent = Intent(this@MainActivity,UsersDetailActivity::class.java)
+            startActivity(intent)
+
+        }
+
+
+
+        setSupportActionBar(binding.toolbar)
+
+        /*val navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)*/
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Create new patient", Snackbar.LENGTH_LONG)
+                .setAction("Action", null)
+                .setAnchorView(R.id.fab).show()
+        }
+    }
+
+    //Example User class
+    data class User(val name: String)
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        /*val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration)*/
+        super.onSupportNavigateUp()
+        return true
+    }
+}
